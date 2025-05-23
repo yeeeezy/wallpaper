6b4b84ff7982c2f7f81d677141e176895d283a37
THANOS <thanos@192.168.0.36>
2025-05-23 22:03:19 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="homeLayout">
+		<view class="banner">
+			<swiper indicator-dots indicator-color="rgba(255,255,255,0.5)" circular indicator-active-color="white" autoplay="true">
+				<swiper-item v-for="item in 3">
+					<image src="/common/images/banner1.jpg" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<view class="notice">
+			<view class="left">
+				<uni-icons type="contact" size="30"></uni-icons>
+			</view>
+			
+			<view class="center"></view>
+			<view class="center"></view>
+			<view class="right">
+				
+			</view>
+		</view>
+	</view>
+	
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss" scoped>
+.homeLayout{
+	.banner{
+		width: 750rpx;
+		padding: 30rpx 0;
+		swiper{
+			width: 750rpx;
+			height: 340rpx;
+			&-item{
+				width: 100%;
+				height: 100%;
+				padding: 0 30rpx;
+				image{
+					width: 100%;
+					height: 100%;
+					border-radius: 10rpx;
+				}
+			}
+		}
+	}
+}
+</style>
---
+++ unpackage/dist/dev/mp-weixin/app.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
+const common_vendor = require("./common/vendor.js");
+if (!Math) {
+  "./pages/index/index.js";
+}
+const _sfc_main = {};
+function _sfc_render(_ctx, _cache) {
+  return {};
+}
+const App = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-f13b4d11"]]);
+function createApp() {
+  const app = common_vendor.createSSRApp(App);
+  return {
+    app
+  };
+}
+createApp().app.mount("#app");
+exports.createApp = createApp;
+//# sourceMappingURL=../.sourcemap/mp-weixin/app.js.map
