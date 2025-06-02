02ee98b9da8eb11ccdcc58394d102d089221dbbd
YingZhiyang <yzysyjz@163.com>
2025-06-02 19:19:43 +0800

    6.2update

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -25,6 +25,45 @@
 				
 			</view>
 		</view>
+		<view class="select">
+			<common-title>
+				<template #name>
+					每日推荐
+				</template>
+				
+				<template #custom>
+					<view class="date">
+						
+						 <uni-icons type="calendar" size="18" color="#28b389"></uni-icons>
+						 <uni-dateformat :date="Date.now()" format="dd日"></uni-dateformat>
+					</view>
+				</template>
+			</common-title>
+			<view class="content">
+				<scroll-view scroll-x="true" >
+					<view class="box" v-for="item in 8 ">
+						<image src="/common/images/preview_small.webp" mode="aspectFill"></image>
+					</view>
+				</scroll-view>
+			</view>
+		</view>
+		
+		<view class="theme">
+			<common-title>
+				<template #name>
+					专题精选
+				</template>
+				<template #custom>
+					<navigator class="more">More+</navigator>
+				</template>
+			</common-title>
+		</view>
+		
+		<view class="content">
+			<theme-item v-for="item in 8">
+				
+			</theme-item>
+		</view>
 	</view>
 	
 </template>
@@ -96,6 +135,54 @@
 			justify-content: center;
 			
 		}
+	}
+	.select{
+		padding-top: 50rpx;
+		.date{
+			color: #28b389;
+			display: flex;
+			align-items: center;
+			.text{
+				margin-left: 5rpx;
+			}
+		}
+		.content{
+			width: 720rpx;
+			margin-top: 30rpx;
+			margin-left: 30rpx;
+			scroll-view{
+				white-space: nowrap;
+				.box{
+					width: 200rpx;
+					height: 430rpx;
+					display: inline-block;
+					margin-right: 15rpx;
+					image{
+						width: 100%;
+						height: 100%;
+						border-radius: 10rpx;
+					}
+				}
+				.box:last-child{
+					margin-right: 30rpx;
+				}
+			}
+		}
+	}
+	
+	.theme{
+		padding-top: 50rpx;
+		.more{
+			font-size: 32rpx;
+			color: #888;
+		}
+	}
+	.content{
+		margin: 30rpx;
+		padding: 0 30rpx;
+		display: grid;
+		gap: 15rpx;
+		grid-template-columns: repeat(3,1fr);
 	}
 }
 </style>
